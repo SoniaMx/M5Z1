8069c6af5b393d9972802682e4a8b18c2f4dae8e
Sonia Matyjaszczyk <aphexsonia@gmail.com>
2024-10-05 16:39:08 +0200

    add M5Z1.py

--- M5Z1.py
+++ M5Z1.py
@@ -18,6 +18,13 @@ class BusinessContact(BaseContact):
     def business_contact(self):
         return f"Wybieram numer {self.work_phone} i dzwonię do {self.name} {self.surname}"
     
+    @property
+    def name_length(self):
+        return len(self.name)
+    @property
+    def surname_length(self):
+        return len(self.surname)
+    
 first_person = BusinessContact(name = "Wanda", surname = "Janik", private_phone = "+48 987345765", email = "ajhjjb@.pl", job_position = "Risk Manager", company_name = "KPMG", work_phone = "+48 123456789")
 second_person = BusinessContact(name = "Jadzia", surname = "Kowalik", private_phone = "+48 865234909", email = "sjsaa@.pl", job_position = "HR Specialist", company_name = "IBM", work_phone = "+48 456321876")
 third_person = BusinessContact(name = "Andrea", surname = "Bilik", private_phone = "+48 555888999", email = "bcvsgd@.pl", job_position = "Payable Supervisor", company_name = "EY", work_phone = "+48 333555444")
@@ -35,3 +42,9 @@ print(second_person.business_contact())
 print(third_person.business_contact())
 print(fourth_person.business_contact())
 print(fifth_person.business_contact())
+#podpunkt 3
+print(f"Ilość liter imienia {first_person.name}: {first_person.name_length}, ilość liter nazwiska {first_person.surname}: {first_person.surname_length}")
+print(f"Ilość liter imienia {second_person.name}: {second_person.name_length}, ilość liter nazwiska {second_person.surname}: {second_person.surname_length}")
+print(f"Ilość liter imienia {third_person.name}: {third_person.name_length}, ilość liter nazwiska {third_person.surname}: {third_person.surname_length}")
+print(f"Ilość liter imienia {fourth_person.name}: {fourth_person.name_length}, ilość liter nazwiska {fourth_person.surname}: {fourth_person.surname_length}")
+print(f"Ilość liter imienia {fifth_person.name}: {fifth_person.name_length}, ilość liter nazwiska {fifth_person.surname}: {fifth_person.surname_length}")
